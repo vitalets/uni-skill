e3f85760ade0d7a8cf33d4edc700aa381276f4f8
Vitaliy Potapov <noginsk@rambler.ru>
2021-09-01 20:04:41 +0300

    marusya: better detect req

--- src/marusya/request.ts
+++ src/marusya/request.ts
@@ -9,9 +9,12 @@ import { State } from '../types/response';
 // Use fake Omit to have 'MarusyaReqBody' in ts messages.
 type MarusyaReqBody = Omit<ReqBody, ''>;
 
+const MARUSYA_APP_TYPES = [ 'mobile', 'speaker', 'VK', 'other' ];
+
 export class MarusyaRequest extends BaseRequest<MarusyaReqBody> implements IRequest<MarusyaReqBody> {
   static match(reqBody: unknown): reqBody is MarusyaReqBody {
-    return Boolean((reqBody as MarusyaReqBody)?.session?.auth_token);
+    const appType = (reqBody as MarusyaReqBody)?.session?.application?.application_type;
+    return Boolean(appType) && MARUSYA_APP_TYPES.includes(appType);
   }
   isMarusya(): this is MarusyaRequest { return true; }
   get userId() {
