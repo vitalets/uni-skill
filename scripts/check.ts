a41c7402a9a6df4dbb705285381fc14649bff143
Vitaliy Potapov <noginsk@rambler.ru>
2021-11-01 21:42:16 +0300

    remove dotenv

--- scripts/check.ts
+++ scripts/check.ts
@@ -2,11 +2,11 @@
  * Debug local skill on real device.
  */
 
-import 'dotenv/config';
 import { runClient } from 'skill-dev-proxy';
+import { SKILL_DEV_PROXY_URL } from '../.env';
 import { handler } from '../playground/skill';
 
 runClient({
-  wsUrl: process.env.SKILL_DEV_PROXY_URL!,
+  wsUrl: SKILL_DEV_PROXY_URL,
   handler,
 });
